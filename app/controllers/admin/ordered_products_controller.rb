7c35ed98a8ee4f8e64b943c27e4592ed6d9617b6
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-06-18 13:32:39 +0000

    Today making

--- app/controllers/admin/ordered_products_controller.rb
+++ app/controllers/admin/ordered_products_controller.rb
@@ -1,5 +1,23 @@
 class Admin::OrderedProductsController < ApplicationController
-  def update
+  def index
+    @orders = Order.all
+  end
+  def show
+    @order = Order.find(params[:id])
+    @ordered_prodacts = @order.ordered_prodacts
   end
 
+  def update
+    @order.change_status!
+    @order_prodacts.change_status!
+  end
+private
+
+  def order_set
+    @order = Order.find(params[:id])
+  end
+ 
+  def ordered_prodact_set
+    @ordered_prodacts = @order.ordered_prodacts
+  end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,23 +1,38 @@
 class Admin::ProductsController < ApplicationController
   def new
+    @product = Product.new
+    @genre = Genre.all
   end
 
   def index
+    @prodacts = Product.page(params[:page]).reverse_order
+    @genre = Genre.all
   end
 
   def create
+     genre.id = product.genre_id
+     @product = Product.new(prodacut_params)
+     @product.save
+     redirect_to  admin_product_path(@product.id)
   end
 
   def show
+     @product = Product.find([:id])
   end
 
   def edit
+     @product = Product.find([:id])
+     
   end
 
   def update
+     @product = Product.find([:id])
+     @product.update
+     redirect_to  admin_product_path(@product.id)
   end
 
   private
-  def 
+  def prodacut_params
+     params.require(:rodacut).permit(:genre_id,:name,:image_id,:description,:price,:product_status)
   end
 end
