aad8df83b5c201b88081a50fb20057c3e4187ef6
katsu-sgt <katsu.sgt.eng@gmail.com>
2021-06-19 02:13:04 +0000

    fix 6/19

--- db/migrate/20210618053422_add_index_to_customers.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToCustomers < ActiveRecord::Migration[5.2]
-  def change
-    change_column :customers, :is_deleted, :boolean, :default => TRUE
-  end
-end
--- db/migrate/20210618122702_change_column_to_customers.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnToCustomers < ActiveRecord::Migration[5.2]
-  def change
-    change_column :customers, :is_deleted, :boolean, :default => FALSE
-  end
-end
