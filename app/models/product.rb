fe7c3855a8a86e5aa57a0852132d1b97df5cb10b
sawabe-sawabe <kapo0515.0515@gmail.com>
2021-06-19 14:41:16 +0000

    today

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,12 @@
 class Product < ApplicationRecord
   has_many :cart_products, dependent: :destroy
   has_many :ordered_products, dependent: :destroy
-  belongs_to :genre
+  belongs_to :genre, optional: true
 
  def  add_tax_price
    (self.price * 1.10).round
  end
   attachment :image
+  enum product_status: { 販売中: true, 販売停止: false }
+  
 end
