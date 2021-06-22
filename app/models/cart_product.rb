89ecd6e08f6f46337b52e1f81e8c4a7628d301f3
katsu-sgt <katsu.sgt.eng@gmail.com>
2021-06-22 06:30:41 +0000

    fix product show

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -1,12 +1,4 @@
 class CartProduct < ApplicationRecord
   belongs_to :customer
   belongs_to :product
-
-  # def total_price
-  #   self.each do |cart_product|
-  #     array << cart_product.product.price
-  #     array.sum
-  #   end
-  # end
-
 end
