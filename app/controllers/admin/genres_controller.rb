e29af9777d1ed99998896441a543a9bd8838c68e
Midori-Gift <okometaberoyo5009@gmail.com>
2021-06-24 02:11:22 +0000

    schemaファイルの更新完了

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -23,18 +23,11 @@ class Admin::GenresController < ApplicationController
     redirect_to new_admin_genre_path
   end
 
-<<<<<<< HEAD
 private
 def genre_params
   params.require(:genre).permit(:name)
 
 end
-=======
-  private
-  def genre_params
-    params.require(:genre).permit(:name)
-  end
->>>>>>> origin/develop
 
 def if_not_admin
   redirect_to admin_session_path unless admin_signed_in?
