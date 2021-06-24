c841a531dab4020947c54a6406a0da081af9291a
Midori-Gift <okometaberoyo5009@gmail.com>
2021-06-24 06:47:03 +0000

    6/24-5

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,5 +1,5 @@
 class Admin::CustomersController < ApplicationController
-
+ before_action :if_not_admin
   def index
      @customer = Customer.page(params[:page]).reverse_order
 
@@ -13,19 +13,6 @@ class Admin::CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
   end
 
-  def unsubscribe
-  end
-
-  def hide
-    @customer = Customer.find(params[:id])
-    if @customer.is_deleted == false
-      @customer.update(is_deleted: true)
-    else
-      @customer.update(is_deleted: false)
-    end
-    redirect_back(fallback_location: root_path)
-  end
-
   def update
     customer = Customer.find(params[:id])
     customer.update(admin_customer_params)
@@ -36,7 +23,9 @@ class Admin::CustomersController < ApplicationController
   def admin_customer_params
      params.require(:customer).permit(:first_name, :last_name, :kana_first_name, :kana_last_name, :postal_code, :address, :phone_number, :email, :is_deleted)
   end
-
+  def if_not_admin
+   redirect_to admin_session_path unless admin_signed_in?
+  end
 
 
 end
