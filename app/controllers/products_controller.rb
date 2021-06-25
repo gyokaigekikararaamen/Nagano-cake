dc959d159e3858072d7bf5e05fd26f4dc41dfce0
savasan2 <83535755+savasan2@users.noreply.github.com>
2021-06-25 07:55:32 +0000

    add design

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,8 +3,15 @@ class ProductsController < ApplicationController
     @products = Product.page(params[:page]).reverse_order
     @products_all = Product.all
   end
-  
+
   def show
     @product = Product.find(params[:id])
   end
+
+  def search
+    @products = Product.search(params[:keyword]).page(params[:page])
+    @keyword = params[:keyword]
+    render "search"
+  end
+
 end
