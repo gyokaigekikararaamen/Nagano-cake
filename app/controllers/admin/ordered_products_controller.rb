20bb4efac19dc7b751f741a2191fba4ddc720cf5
katsu-sgt <katsu.sgt.eng@gmail.com>
2021-06-25 05:06:16 +0000

    fix admin ordere

--- app/controllers/admin/ordered_products_controller.rb
+++ app/controllers/admin/ordered_products_controller.rb
@@ -6,15 +6,22 @@ class Admin::OrderedProductsController < ApplicationController
   def show
 
   end
+  
   def update
-    @order_products = OrderProducts.find(params[:id])
-     if@product.update(post_order_producut_params)
-     redirect_to admin_order_path(@order.id)
+    @order_product = OrderProducts.find(params[:id])
+     if @order_product.update(ordered_producut_params)
+       redirect_to admin_order_path(@order.id)
      else
-     render :show
+       render :show
      end
   end
+  
  private
+ 
+ def ordered_product_params
+   params.require(:order_products).permit(:production_status)
+ end
+ 
  def if_not_admin
    redirect_to admin_session_path unless admin_signed_in?
  end
