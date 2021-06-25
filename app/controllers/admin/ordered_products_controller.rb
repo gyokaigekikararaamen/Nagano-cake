77ba03991eb2da0fcf45b13358af546aedb134bf
Midori-Gift <okometaberoyo5009@gmail.com>
2021-06-25 08:26:48 +0000

    6/25-4

--- app/controllers/admin/ordered_products_controller.rb
+++ app/controllers/admin/ordered_products_controller.rb
@@ -9,7 +9,7 @@ class Admin::OrderedProductsController < ApplicationController
   def update
      @order=Order.find(params[:id])
     @order_products = OrderedProduct.find(params[:id])
-     if@order_products.update( ordered_product_params)
+     if@order_products.update(ordered_product_params)
      redirect_to admin_order_path(@order.id)
      else
      render :show
@@ -19,7 +19,7 @@ class Admin::OrderedProductsController < ApplicationController
  def if_not_admin
    redirect_to admin_session_path unless admin_signed_in?
  end
- 
+
   def ordered_product_params
    params.require(:ordered_product).permit(:production_status)
   end
